78190915fdecb0042cf5b67b49c7ee321c0a599b
Allan kevyn <aksilvasouza@hotmail.com>
2022-02-16 23:02:19 -0300

    implemented to work again

---
+++ app/src/main/java/com/example/pokemonapp/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.pokemonapp
+
+import com.example.pokemonapp.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel  @Inject internal constructor() : BaseViewModel() {
+}
---
+++ app/src/main/java/com/example/pokemonapp/presentation/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.pokemonapp.presentation.home
+
+import com.example.pokemonapp.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+class HomeViewModel @Inject constructor() : BaseViewModel() {
+
+}
