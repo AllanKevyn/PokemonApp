24532c401d0645f87a24015d48c1fcb01d227b5b
Allan Kevyn <aksilvasouza@hotmail.com>
2022-03-04 00:56:32 -0300

    repair

--- app/src/main/java/com/example/pokemonapp/util/ProgressBars.kt
+++ app/src/main/java/com/example/pokemonapp/util/ProgressBars.kt
@@ -3,9 +3,10 @@ package com.example.pokemonapp.util
 import android.animation.ObjectAnimator
 
 object ProgressBars {
+
      fun setProgressBar(target: Any, progressKey: String, from: Int, data: Int ){
         val progressAnimator = ObjectAnimator.ofInt(target, progressKey, from, from + data)
-        progressAnimator.setDuration(1000)
+         progressAnimator.duration = 1000
         progressAnimator.start()
     }
 }
